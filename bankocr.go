c849f84fa5d9dc53a5726e084b773ea63275ca31
Kevin Lau <kckl@dropbox.com>
2022-05-13 17:00:58 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bankocr.go

--- bankocr.go
+++ bankocr.go
@@ -1,6 +1,31 @@
 package bankocr
 
+import "strings"
+
 // Parse returns a numeric string for the input.
 func Parse(input string) string {
-	return input
+	lines := strings.Split(input, "\n")
+	var digit_lines [][]string
+	for i := 0; i < len(lines); i += 3 {
+		digit_lines = append(digit_lines, []string{lines[i], lines[i+1], lines[i+2]})
+	}
+	// For each entry
+	result := ""
+	for _, digit_line := range digit_lines {
+		for i := 0; i < 9; i++ {
+			result += getDigitString(digit_line, i)
+		}
+		result += "\n"
+	}
+}
+
+func getDigitString(input []string, idx int) string {
+	result := ""
+	for _, line := range input {
+		for i := 0; i < 3; i++ {
+			result += string(line[i+idx*3])
+		}
+		result += "\n"
+	}
+	return result
 }
--- bankocr_test.go
+++ bankocr_test.go
@@ -15,3 +15,15 @@ func TestParseZeros(t *testing.T) {
 		t.Errorf("Parse(%v) = %v; want %v", in, out, want)
 	}
 }
+
+func TestParseNumbers(t *testing.T) {
+	in := `
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|`
+	want := "123456789"
+	out := Parse(in)
+	if out != want {
+		t.Errorf("Parse(%v) = %v; want %v", in, out, want)
+	}
+}
